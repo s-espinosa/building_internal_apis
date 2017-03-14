3cf91dfd4e370a879d3a93730ab4c32a12b8aaeb
Sal Espinosa <sespinos@gmail.com>
2017-03-14 10:56:21 -0600

    Jbuilder complete

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::OrdersController < ApplicationController
   def index
-    render json: Order.all
+    @orders = Order.all
   end
 
   def show
-    render json: Order.find(params[:id])
+    @order = Order.find(params[:id])
   end
 end
